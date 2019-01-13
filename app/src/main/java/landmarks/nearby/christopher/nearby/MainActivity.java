2308cd3a1a142e9a798b01c4184cd4508fe4ac7a
Christopher <czaremba@outlook.com>
2019-01-12 23:38:59 -0500

    Removed FusedLocationProviderClient as the method to detect location and am instead using LocationManager to obtain GPS location. Still either doesn't return location or takes a long time to get GPS location.

--- app/src/main/java/landmarks/nearby/christopher/nearby/MainActivity.java
+++ app/src/main/java/landmarks/nearby/christopher/nearby/MainActivity.java
@@ -1,11 +1,15 @@
 package landmarks.nearby.christopher.nearby;
 
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
@@ -14,7 +18,9 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,15 +28,51 @@ import com.google.android.gms.tasks.Task;
 public class MainActivity extends AppCompatActivity {
 
     static LocationManager myLocationManager;
+    static LocationCallback myLocationCallback;
 
 
+    @SuppressLint("MissingPermission")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
 
-        FusedLocationProviderClient providerClient = LocationServices.getFusedLocationProviderClient(this);
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+        if(!havePermissions()){
+            return;
+        }
+
+        //Use GPS to get location
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                Toast.makeText(getApplicationContext(), "Location Long: " + location.getLongitude() + " Location Lat: "
+                + location.getLatitude(), Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        });
+
+
+    }
+
+    public boolean havePermissions(){
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
@@ -39,13 +81,8 @@ public class MainActivity extends AppCompatActivity {
             //                                          int[] grantResults)
             // to handle the case where the user grants the permission. See the documentation
             // for ActivityCompat#requestPermissions for more details.
-            return;
+            return false;
         }
-        providerClient.requestLocationUpdates(new LocationRequest(), null).addOnCompleteListener(new OnCompleteListener<Void>() {
-            @Override
-            public void onComplete(@NonNull Task<Void> task) {
-                Toast.makeText(getApplicationContext(), "Result: "+task.getResult(), Toast.LENGTH_LONG).show();
-            }
-        });
+        return true;
     }
 }
